00c3d5d43e0bc9749a288d73aadfd3271783c1d8
Ayrat <axmetxan@gmail.com>
2023-05-25 13:11:06 +0300

    fix: token expire redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import { Route, Routes } from 'react-router';
-import { useEffect } from 'react';
 import MainPage from './pages/main-page/MainPage';
 import Page404 from './pages/page-404/Page404';
 import LoginPage from './pages/login-page/LoginPage';
 import RegisterPage from './pages/register-page/RegisterPage';
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from './components/Layout';
-import useTokenExpiration from './hooks/useTokenExpiration';
-import { useAppDispatch } from './hooks/hooks';
-import { removeUser } from './services/slices/userDataSlice';
 import WelcomePage from './pages/welcome-page/WelcomePage';
 
 const App = () => {
-  const isTokenExpired = useTokenExpiration();
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (isTokenExpired) {
-      dispatch(removeUser());
-    }
-  }, [dispatch, isTokenExpired]);
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,13 +1,14 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { CircularProgress } from '@mui/material';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import DocumentationSection from '../../components/DocumentationSection/DocumentationSection';
 import styles from './MainPage.module.css';
 import QueryEditorSection from '../../components/QueryEditorSection/QueryEditorSection';
 import ResponseDataSection from '../../components/ResponseDataSection copy/ResponseDataSection';
 import { useGetGraphQLDataMutation } from '../../services/rickandmortyAPI';
 import useAuth from '../../hooks/useAuth';
-import { useAppDispatch } from '../../hooks/hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import useTokenExpiration from '../../hooks/useTokenExpiration';
 import { removeUser } from '../../services/slices/userDataSlice';
 
 const MainPage = () => {
@@ -15,25 +16,29 @@ const MainPage = () => {
     fixedCacheKey: 'shared-graphQL-data',
   });
   const { isAuth } = useAuth();
+  const { token } = useAppSelector((state) => state.userData);
+  const isTokenExpired = useTokenExpiration();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token !== null && isTokenExpired) {
+      dispatch(removeUser());
+      navigate('/welcome');
+    }
+  }, [dispatch, isTokenExpired, navigate, token]);
 
   return isAuth ? (
-    <main className={styles.mainContainer}>
-      {isGraphQLDataRequestSucces && (
-        <Suspense fallback={<CircularProgress />}>
-          <DocumentationSection />
-        </Suspense>
-      )}
-      <QueryEditorSection />
-      <ResponseDataSection />
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(removeUser());
-        }}
-      >
-        Sign Out
-      </button>
+    <main className="main">
+      <div className={styles.mainPageContainer}>
+        {isGraphQLDataRequestSucces && (
+          <Suspense fallback={<CircularProgress />}>
+            <DocumentationSection />
+          </Suspense>
+        )}
+        <QueryEditorSection />
+        <ResponseDataSection />
+      </div>
     </main>
   ) : (
     <Navigate to="/login" replace />
